47b876dfbe9edb9061a3f16f1954a9273e927198
Gabriel Oliveira <bielgabrieloliveira77@gmail.com>
2024-08-13 23:35:05 +0000

    Feat: first commit with architecture

---
+++ src/test/java/com/cotuca/CotucaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cotuca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CotucaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
